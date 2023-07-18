2a77d0b9a8d725b2edd404415707e871d1d89771
Dennis Priskorn <egils@riseup.net>
2023-07-18 16:10:49 +0200

    chore: First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# LantmäterietHikingPaths
+Script to extract, filter and convert hiking paths from the 
+product Topografi 50 from swedish state Lantmäteriet to WSG84 
+geojson easily usable in JOSM
+
+It was made with help from chatgpt may 24 2023.
---
+++ extract_hiking_paths.py
@@ -0,0 +1,43 @@
+import argparse
+import geopandas as gpd
+from pyproj import CRS
+
+# Create the argument parser
+parser = argparse.ArgumentParser(description="Convert GeoPackage coordinates from SWEREF99TM to WGS84 and export to GeoJSON")
+
+# Add the input and output file arguments
+parser.add_argument("input_file", help="Path to the input GeoPackage file")
+parser.add_argument("output_file", help="Path to the output GeoJSON file")
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Input and output file paths
+input_gpkg = args.input_file
+output_geojson = args.output_file
+
+print("Reading gpkg")
+
+layer_name = "ovrig_vag"
+
+# Read the GeoPackage into a GeoDataFrame
+gdf = gpd.read_file(gpkg, layer=layer_name)
+
+# Filter the GeoDataFrame based on the "objekttypnr" attribute
+filtered_gdf = gdf[gdf["objekttypnr"] == 1846]
+
+# Define the target coordinate reference system (CRS)
+target_crs = CRS.from_epsg(4326)  # WGS84
+
+# Set the CRS attribute on the GeoDataFrame
+filtered_gdf.crs = CRS.from_epsg(3006)  # SWEREF99TM
+
+# Transform the coordinates to the target CRS
+transformed_gdf = filtered_gdf.to_crs(crs=target_crs)
+
+# Print the transformed GeoDataFrame
+print(transformed_gdf)
+
+# Save the filtered features to a new GeoJSON file
+print("Writing geojson")
+transformed_gdf.to_file(output_geojson, driver="GeoJSON")
